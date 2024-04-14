d73912db9a43a4b03045db3d451d5bc7d7e2886a
Naveed Ausaf <naveed.ausaf@gmail.com>
2024-04-14 17:38:29 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        latoRegular: "Lato-Regular",
+        latoBold: "Lato-Bold",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
